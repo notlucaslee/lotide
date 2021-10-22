d2ef61bb869d76ee6d4f51beea4ed53d1b510f8b
Lucas Lee <lee.lucas.hyunguk@gmail.com>
2021-10-22 02:41:11 +0000

    returning first item in array

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x1F975)}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x1F976)}Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = array => array[0];
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
