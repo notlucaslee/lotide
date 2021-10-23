928a87b9baa8bdaa089e7ec0522d1300d6458f67
Lucas Lee <lee.lucas.hyunguk@gmail.com>
2021-10-23 06:32:33 +0000

    equal arrays

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const eqArrays = function(array1, array2) {
+  let checkString = '';
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array2.length; i++) {
+      if (array1[i] === array2[i]) {
+        checkString += '1';
+      } else {
+        checkString += '0';
+      }
+    }
+  }
+  if (checkString.includes('0')) { 
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x1F975)}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x1F976)}Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
