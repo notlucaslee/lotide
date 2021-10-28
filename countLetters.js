75832a43e858e0d4cda27c22b4f2d894ff9a48a2
Lucas Lee <lee.lucas.hyunguk@gmail.com>
2021-10-28 04:26:41 +0000

    countLetters function

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x1F975)}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x1F976)}Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(word) {
+  let result = {};
+  for (let letter of word) {
+    if (result[letter] === undefined) {
+      result[letter] = 1;
+    } else {
+      result[letter] += 1
+    }
+  }
+  delete result[' '];
+  return result;
+}
+
+result1 = countLetters("lighthouse in the house");
+
+assertEqual(result1.l, 1);
+assertEqual(result1.i, 2);
+assertEqual(result1.h, 4);
