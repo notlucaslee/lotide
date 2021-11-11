6110db24f68c2312c9003035ce21670b74d38fb5
notlucaslee <lee.lucas.hyunguk@gmail.com>
2021-11-11 06:34:28 +0000

    turning lotide into oop

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,29 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const expect = require('chai').expect;
 
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle(['how', 'many', 'miles', 'must', 'a', 'man', 'walk', 'down']), ['must', 'a']);
+// assertArraysEqual(middle([1]),[]);
+// assertArraysEqual(middle([1, 2]),[]);
+// assertArraysEqual(middle([1, 2, 3]),[2]);
+// assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
+// assertArraysEqual(middle(['how', 'many', 'miles', 'must', 'a', 'man', 'walk', 'down']), ['must', 'a']);
+
+describe ('middle function', () => {
+
+  it('returns blank array when array of one item is passed in', () => {
+    expect(middle([1])).to.deep.equal([]);
+  });
+
+  it('returns blank array when array of two items is passed in', () => {
+    expect(middle([1, 2])).to.deep.equal([]);
+  });
+
+  it('returns [2] when [1, 2, 3] is passed in', () => {
+    expect(middle([1, 2, 3])).to.deep.equal([2]);
+  });
+
+  it('returns middle two items when array of even # of items passed in', () => {
+    expect(middle([1, 2, 3, 4]),[2, 3]).to.deep.equal([2, 3]);
+  });
+  
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,17 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const expect = require('chai').expect;
+ //const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// describe('tail function', function () {
+//   it("should return [\'Lighthouse\', \'Labs\']", function() {
+//     const words = ["Yo Yo", "Lighthouse", "Labs"];
+//     assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+//   });
+// });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe('tail function', function () {
+  it('should return [\'Lighthouse\', \'Labs\']', function() {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    expect(tail(words)).to.deep.equal(['Lighthouse', 'Labs']);
+  });
+});
