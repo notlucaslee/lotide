00de47cdbd7097677cdeab7adae688028afa8150
notlucaslee <lee.lucas.hyunguk@gmail.com>
2021-11-13 06:23:08 +0000

    final lotide submission

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+
 
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  flatten,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
+  letterPositions
 };
